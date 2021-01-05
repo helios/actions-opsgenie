e6246409127a0ab1a5e10385fa61f0ef680d3d9a
Matt Silbernagel <matt@silbernagel.dev>
2021-01-05 14:16:47 -0500

    another attempt

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,7 +53,7 @@ STATUS_CODE=$(curl -s \
             \"alias\": \"${ALIAS}\",
             \"message\": \"${MESSAGE}\",
             \"priority\": \"${PRIORITY}\",
-            \"tags\": ${TAGS}
+            \"tags\": [\"${TAGS}\"]
         }")
 
 # Validate status code
